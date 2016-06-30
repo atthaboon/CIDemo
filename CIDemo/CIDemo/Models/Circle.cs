d0f9f9739ffa92e0fcb921b6ec979755c3299a5e
Atthaboon.s <art.a@excelbangkok.com>
2016-06-30 16:41:07 +0700

    Init project

---
+++ CIDemo/CIDemo/Models/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDemo.Models
+{
+    public class Circle : Shape
+    {
+        public Circle(double width, double height) : base(width, height)
+        {
+        }
+
+        public override double CalculateSurface()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CIDemo/CIDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using CIDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Automate Shape Workshop");
+
+            var rectangle = new Rectangle(5, 4);
+            // Console.WriteLine($"Rectangle surface is {rectangle.CalculateSurface()}");
+
+            var triangle = new Triangle(5, 4);
+            // Console.WriteLine($"Triangle surface is {triangle.CalculateSurface()}");
+
+            var circle = new Circle(5, 4);
+            // Console.WriteLine($"Circle surface is {circle.CalculateSurface()}");
+
+            Console.ReadLine();
+        }
+    }
+}
