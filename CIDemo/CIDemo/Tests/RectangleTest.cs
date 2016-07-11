f31f292a148eef9a728e1f551ed884b7ab081388
Mongkon Eiadon <liie.m@excelbangkok.com>
2016-07-11 17:17:15 +0700

    MIE Add TestCodes

---
+++ CIDemo/CIDemo/Tests/RectangleTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CIDemo.Models;
+using NUnit.Framework;
+
+namespace CIDemo.Tests
+{
+    [TestFixture]
+    public class RectangleTest
+    {
+        private IShapeCalculate ShapCalculate;
+        public void Setup()
+        {
+            ShapCalculate  = new Rectangle();   
+        }
+
+        [Test]
+        public void Rectangle_Calculation()
+        {
+            var width = 240;
+            var height = 360;
+
+            var actual = ShapCalculate.CalculateSurface(width, height);
+
+            Assert.AreEqual(width*height, actual);
+        }
+
+    }
+}
